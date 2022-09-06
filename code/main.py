032856fe21c65587f7c8580537ae69f25764f393
Farid Qamar <qamar@kalamazoo.local>
2022-09-06 12:39:54 -0400

    created main python file

---
+++ code/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Main driver code for the paper titled "Pixel-Wise Classification of 
+High-Resolution Ground-Based Urban Hyperspectral Images with 
+Convolutional Neural Networks"
+
+Author: Farid Qamar
+Organizartion: Urban Observatory, University of Delaware
+
+"""
+
+# import python libraries
+import sys
+import pandas as pd
+import numpy as np
+
+# import local libraries
+import params
+import functions as fn
+
+print("TensorFlow Version:", tf. __version__)
+print("Python Version:", version)
+
+
+
